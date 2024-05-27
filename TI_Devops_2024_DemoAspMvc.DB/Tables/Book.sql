4a72c4bc54c4aa998e48d3cdfc15c720e0fb7774
Sebastien <byasebastien@hotmail.com>
2024-05-27 09:37:29 +0200

    Add project files.

---
+++ TI_Devops_2024_DemoAspMvc.DB/Tables/Book.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Book]
+(
+	[ISBN] VARCHAR(13) PRIMARY KEY,
+	[Title] VARCHAR(100) NOT NULL,
+	[Description] VARCHAR(MAX) NULL,
+	[Publish_date] DATETIME2 NOT NULL,
+	[Author_id] INT NOT NULL,
+	CONSTRAINT FK_Book_Author FOREIGN KEY (Author_id)
+		REFERENCES Author(Id),
+	CONSTRAINT UK_Book UNIQUE (Title,Publish_date,Author_id)
+)
